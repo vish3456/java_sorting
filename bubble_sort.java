5a004448510e15dfc418ee2576bfa468f971707a
Vishnu Singh <vishnu.singh_cs.h24@gla.ac.in>
2025-10-09 20:40:36 +0530

    bubble sort

---
+++ bubble_sort.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class bubble_sort {
+    public  static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            boolean swap = false;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swap = true;
+                }
+            }
+            if(!swap) break;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
