9d33c2baee10cdc3ef2883d8667089bafb8f745f
Vishnu Singh <vishnu.singh_cs.h24@gla.ac.in>
2025-10-22 22:10:49 +0530

    SelectionSort

---
+++ Selectionsort.java
@@ -0,0 +1,27 @@
+// here , we pick the  smallest element , and then swap it.
+import java.util.*;
+public class Selectionsort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        for(int i=0;i<n-1;i++){
+            int min = i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min]){
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+
+        }
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+}
